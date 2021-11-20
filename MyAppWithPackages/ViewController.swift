b0a7753381d38e3f5e1f47e72e6ef5c2334ce7e0
Brunno Braga <brunnobga@gmail.com>
2021-11-19 21:13:11 -0300

    sample project

---
+++ MyAppWithPackages/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  MyAppWithPackages
+//
+//  Created by Brunno Alberto Braga on 19/11/21.
+//
+
+import UIKit
+import Apptimize
+import RxCoreData
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+    let x = 10
+    print(x)
+  }
+
+
+}
+
